51a62f9fd835a5c0e83c620e4ba8f52be3f65dad
Liubofu <luboff1992@gmail.com>
2026-01-13 21:47:24 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,50 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задание 1!");
 
+        int[] num = new int[3];
+        num[0] = 1;
+        num[1] = 2;
+        num[2] = 3;
+        float[] num2 = {1.57f, 7.654f, 9.986f};
+        int[] num3 = new int[7];
+        num3[0] = 1;
+        num3[1] = 2;
+        num3[2] = 3;
+        num3[3] = 10;
+        num3[4] = 8;
+        num3[5] = 25;
+        num3[6] = 7;
+
+        System.out.println("Задание 2!");
+
+        System.out.println(Arrays.toString(num));
+        System.out.println(Arrays.toString(num2));
+        System.out.println(Arrays.toString(num3));
+
+        System.out.println("Задание 3!");
+
+        for (int i = num.length - 1; i >= 0; i--) {
+            System.out.println(num[i]);
         }
+        for (float i = num2.length - 1; i >= 0; i--) {
+            System.out.println(num2[(int) i]);
+        }
+        for (int i = num3.length - 1; i >= 0; i--) {
+            System.out.println(num3[i]);
+        }
+
+        System.out.println("Задание 4!");
+
+        for (int n = 0; n < num.length; n++) {
+            if (num[n] % 2 != 0) {
+                num[n] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(num)); //num[0] = 1; num[1] = 2; num[2] = 3;
     }
 }
