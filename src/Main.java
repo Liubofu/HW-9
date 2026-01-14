61cbeb6e2861ab17cf6a07ec6fd65578dfb21aca
Liubofu <luboff1992@gmail.com>
2026-01-14 22:24:28 +0300

    Исправление задачи 3 (запятые)

--- src/Main.java
+++ src/Main.java
@@ -48,8 +48,8 @@ public class Main {
 
         System.out.println("Задание 3!");
 
-        for (int i = num.length - 1; i >= 0; i--) {
-            if (i > 0) {
+        for (int i = num.length-1; i >= 0; i--) {
+            if (i >= 0) {
                 System.out.print(num3[i]);
                 System.out.print(", ");
             }
